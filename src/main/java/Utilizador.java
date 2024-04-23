91e0b7a35ff7eb258531334d93e16fc95f86402c
Rafael Rosa <rafaelrosa1004@hotmail.com>
2024-04-23 11:50:07 +0100

    User feito acho eu

--- src/main/java/Utilizador.java
+++ src/main/java/Utilizador.java
@@ -2,6 +2,20 @@ public class Utilizador {
     private int passwordHash;
     private String username;
 
+    public int getPasswordHash() {
+        return passwordHash;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        passwordHash = password.hashCode();
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public Utilizador(String nome, String password){
         username = nome;
         passwordHash = password.hashCode();
