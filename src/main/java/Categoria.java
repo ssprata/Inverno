7407e90b547b64fbcdc69f2e1d3e42ff3ba60f46
Rafael Rosa <rafaelrosa1004@hotmail.com>
2024-04-23 11:24:55 +0100

    Categoria quase feita

--- src/main/java/Categoria.java
+++ src/main/java/Categoria.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class Categoria {
     public static final int UNIDADE = 0;
     public static final int KILO = 1;
@@ -5,12 +7,16 @@ public class Categoria {
 
     private String nome;
     private int unit;
+    private LinkedList<Produto> produtos;
 
     public String getNome() {
         return nome;
     }
     public int getUnit() {
         return unit;
+    }
+    public LinkedList<Produto> getProdutos() {
+        return produtos;
     }
 
     public void setNome(String nome) {
@@ -19,9 +25,21 @@ public class Categoria {
     public void setUnit(int unit) {
         this.unit = unit;
     }
+    public void setProdutos(LinkedList<Produto> produtos) {
+        this.produtos = produtos;
+    }
 
     public Categoria(String nome, int unit) {
         this.nome = nome;
         this.unit = unit;
+    }
+
+    public void addProduto(Produto produto){
+        for(Produto p : produtos){
+            if(p.getNome() == produto.getNome()){
+                throw new IllegalArgumentException("Produto não pode ter o mesmo nome que um já existente");
+            }
+        }
+        produtos.add(produto);
     }
 }
--- src/main/java/Produto.java
+++ src/main/java/Produto.java
@@ -1,2 +1,18 @@
 public class Produto {
+    private String nome;
+    private int quantidade;
+
+    public String getNome() {
+        return nome;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
