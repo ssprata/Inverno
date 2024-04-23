2cb96e4f90ae116c91029d610e68800eeab76136
Rafael Rosa <rafaelrosa1004@hotmail.com>
2024-04-23 13:03:34 +0100

    Login começo

---
+++ src/main/java/LoginUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class LoginUI {
+    private JPanel background;
+    private JLabel Title;
+    private JPanel buttonPanel;
+    private JTextField textFieldNome;
+    private JTextField textFieldPassword;
+    private JButton buttonCheck;
+}
